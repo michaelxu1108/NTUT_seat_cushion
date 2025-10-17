69d658baec5367e9d2ddf4a28f6a2350488ec450
MichaelXu <yjo494512@gmail.com>
2025-10-17 21:17:27 +0800

    刪除無用專案資料

--- apps/ad5940_temp_debugger/android/app/src/main/kotlin/com/example/ad5940_temp_debugger/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ad5940_temp_debugger
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- apps/ad5940_temp_debugger/lib/presentation/view/bluetooth_devices_scanner/bluetooth_devices_scanner.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bluetooth_presentation/bluetooth_presentation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart' as fbp;
-import 'package:provider/provider.dart';
-
-part 'controller.dart';
-part 'theme.dart';
-part 'view.dart';
